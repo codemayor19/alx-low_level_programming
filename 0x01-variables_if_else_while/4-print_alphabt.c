e1d4578f0e5fb9ec3aec5a82ffc44e0ac39b3530
codemayor19 <codemayor19@gmail.com>
2023-09-08 07:10:41 -0700

    new commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	do {
+		if (alpha != 'e'|| alpha != 'q')
+		{
+			putchar(alpha);
+			alpha++;
+		}
+	} while (alpha <= 'z');
+	putchar('\n');
+	return (0);
+}
