363d248a58db117f610e7b516420a4e2616b55ab
codemayor19 <codemayor19@gmail.com>
2023-09-08 17:03:42 -0700

    new commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+	do {
+		j = i + 1;
+		do {
+			k = j + 1;
+			do {
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+					k++;
+				} else {
+					putchar('\n');
+					return (0);
+				}
+			} while (k <= 9);
+			j++;
+		} while (j <= 8);
+		i++;
+	} while (i <= 7);
+}
