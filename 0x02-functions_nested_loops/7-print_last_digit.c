1e5b5860410e1fe0fd1a5f9b5df803103415264b
codemayor19 <codemayor19@gmail.com>
2023-09-12 10:12:50 -0700

    new commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n = (n * -1) % 10;
+		n = (n % 10) * -1;
 		_putchar('0' + n);
 		return (n);
 	}
-	n = (n * +1) % 10;
+	n = n % 10;
 	_putchar('0' + n);
 	return (n);
 }
---
+++ 0x02-functions_nested_loops/check_max.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("int min is %d", INT_MIN);
+}
