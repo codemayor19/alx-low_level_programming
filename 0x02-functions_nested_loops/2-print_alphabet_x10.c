292c07c8ed02090fa6676ca2d1d02e31aeabd069
codemayor19 <codemayor19@gmail.com>
2023-09-12 06:35:12 -0700

    new commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	j = 0;
+	while (j < 10)
+	{
+		i = 'a'
+		while (i <= 'z')
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
