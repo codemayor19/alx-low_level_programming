3ebc01cf2cb40039cec6fb29053274ac291cfcee
codemayor19 <codemayor19@gmail.com>
2023-09-12 07:22:04 -0700

    new commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - validate if sign
+ * @n: if number is positive or negative
+ *
+ * Return: 1 of number is positive
+ * 0 if number is zero
+ * -1 if number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-1');
+	return (-1);
+}
