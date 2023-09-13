a791515b9689b2cba0d9944c73537bfe98e8812c
codemayor19 <codemayor19@gmail.com>
2023-09-13 00:04:40 -0700

    new commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,16 @@ void times_table(void)
 			n = i * j;
 			if (n == 0)
 			{
-				_putchar('0');
+				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0');
+				} else
+				{
+					_putchar('0');
+				}
 			} else if (n < 10)
 			{
 				_putchar(',');
