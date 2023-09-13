c21cbe4ca98bac1e5222f8ee65e6f53da4ae21f5
codemayor19 <codemayor19@gmail.com>
2023-09-13 00:53:28 -0700

    new commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print n to 98
+ * @n: starting point
+ *
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n");
+	} else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d\n");
+	}
+}
