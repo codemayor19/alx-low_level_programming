92d6894ec4bfbabcf80c97f8d761d53b342bc114
codemayor19 <codemayor19@gmail.com>
2023-09-13 01:25:23 -0700

    new commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - print times table
+ * @n: value
+ *
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n < 0 || n > 15)
+	{
+		i = 0;
+		while (i <= n)
+		{
+			j = 0;
+			while (j <= n)
+			{
+				k = j * n;
+				if (k == 0)
+				{
+					if (j > 0)
+					{
+						printf(",   %d", k);
+					} else
+					{
+						_putchar('0');
+					}
+				} else if (k >= 10)
+				{
+					printf(",  %d", k);
+				} else if (k >= 100)
+				{
+					printf(", %d", k);
+				} else
+				{
+					printf(",   %d", k);
+				}
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
