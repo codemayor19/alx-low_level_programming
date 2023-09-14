beeafb4826c0c732cbf45923375d2fb78a947185
codemayor19 <codemayor19@gmail.com>
2023-09-14 07:27:24 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ * @n: value
+ *
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	} else
+	{
+		num = n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
