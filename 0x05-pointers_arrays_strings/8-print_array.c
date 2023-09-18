5ad94f2ec4dcf64e4c2f867b413b55ba173991d6
codemayor19 <codemayor19@gmail.com>
2023-09-18 14:10:17 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array
+ * @a - array values
+ * @n - amt of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		} else
+		{
+			printf(", %d", a[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
