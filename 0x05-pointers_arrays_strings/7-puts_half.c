b55e1afa4db85f9a5efd7853ef52bfc69ccbe2ea
codemayor19 <codemayor19@gmail.com>
2023-09-18 12:48:59 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - puts half
+ * @str: string half
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	for (j = i / 2; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
