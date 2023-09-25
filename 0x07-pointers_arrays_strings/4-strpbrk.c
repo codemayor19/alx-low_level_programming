016cd296b395810367c5e5501fcfd13d7ec1a750
codemayor19 <codemayor19@gmail.com>
2023-09-25 15:05:49 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - strp break
+ * @s: string
+ * @accept: accept
+ *
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
