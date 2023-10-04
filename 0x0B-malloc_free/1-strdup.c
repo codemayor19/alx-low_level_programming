443091e79b3fed563c4f5aef134846bc29c868a5
codemayor19 <codemayor19@gmail.com>
2023-10-03 23:48:48 -0700

    new commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - return a copy of th string
+ * @str: string value
+ *
+ * Return: string value
+ */
+char *_strdup(char *str)
+{
+	int size, i;
+	char *ptrArray;
+
+	i = 0;
+	size = 0;
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptrArray = malloc(size * sizeof(char));
+	if (ptrArray == NULL)
+	{
+		return (NULL);
+	}
+	while (i <= size)
+	{
+		ptrArray[i] = str[i];
+		i++;
+	}
+	return (ptrArray);
+}
