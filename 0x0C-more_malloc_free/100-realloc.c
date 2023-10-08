fa22b43c03094b04cdc2f47d4897f5ecdcc7ad82
codemayor19 <codemayor19@gmail.com>
2023-10-08 12:40:30 -0700

    new commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,12 +8,12 @@
  * @old_size: int
  * @new_size: int
  *
+ * Return: Pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	char *p;
-	char *pt = ptr;
+	char *p, *pt = ptr;
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -30,9 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 		return (p);
 	} else if (new_size == old_size)
-	{
 		return (ptr);
-	}
 	p = malloc(new_size);
 	if (p == NULL)
 	{
@@ -41,21 +39,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size > old_size)
 	{
-		i = 0;
-		while (i < old_size)
-		{
+		for (i = 0; i < old_size; i++)
 			p[i] = pt[i];
-			i++;
-		}
 		free(ptr);
 		return (p);
 	}
-	i = 0;
-	while (i < new_size)
+	for (i = 0; i < new_size; i++)
 	{
 		p[i] = pt[i];
-		i++;
-	}
+	};
 	free(ptr);
 	return (p);
 }
