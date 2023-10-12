0205e745ba35199cc2f7022935ab5ef031cf1ab5
codemayor19 <codemayor19@gmail.com>
2023-10-12 12:46:46 -0700

    new commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - array rntry
+ * @array: array
+ * @size: size
+ * @action: fubction pointer
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if (array == NULL  || action == NULL || size <= 0)
+		return;
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
