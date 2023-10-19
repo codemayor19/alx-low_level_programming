4e024f1fddc4042a6392e12879a37aac6752caad
codemayor19 <codemayor19@gmail.com>
2023-10-19 13:41:45 -0700

    new commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * print_list - print list
+ * @h: struct
+ *
+ * Return: length
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0, j;
+	char *_null = "(nil)";
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			_putchar('[');
+			_putchar('0');
+			_putchar(']');
+			_putchar(' ');
+			for (j = 0; _null[j] != '\0'; j++)
+			{
+				_putchar(_null[j]);
+			}
+		} else
+		{
+			_putchar('[');
+			_putchar(h->len + '0');
+			_putchar(']');
+			_putchar(' ');
+			for (j = 0; h->str[j] != '\0'; j++)
+			{
+				_putchar(h->str[j]);
+			}
+		}
+		_putchar('\n');
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
