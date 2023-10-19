90b769e2101dca61ea724dc243adec288a86f6ed
codemayor19 <codemayor19@gmail.com>
2023-10-19 14:18:14 -0700

    new commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,6 +18,8 @@ typedef struct list_s
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
