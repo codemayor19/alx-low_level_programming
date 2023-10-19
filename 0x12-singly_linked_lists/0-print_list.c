fd3c71ffb540fa4d8548ebcc06f5558981337386
codemayor19 <codemayor19@gmail.com>
2023-10-19 14:16:34 -0700

    new commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,33 +9,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-	int j;
-	char *_null = "(nil)";
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			_putchar('[');
-			_putchar('0');
-			_putchar(']');
-			_putchar(' ');
-			for (j = 0; _null[j] != '\0'; j++)
-			{
-				_putchar(_null[j]);
-			}
-		} else
-		{
-			_putchar('[');
-			_putchar(h->len + '0');
-			_putchar(']');
-			_putchar(' ');
-			for (j = 0; h->str[j] != '\0'; j++)
-			{
-				_putchar(h->str[j]);
-			}
-		}
-		_putchar('\n');
+			printf("[%d] %s", 0, "(nil)");
+		 else
+			printf("[%d] %s\n", h->len, h->str)
 		i++;
 		h = h->next;
 	}
