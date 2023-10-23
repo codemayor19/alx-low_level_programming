ef391e1c7484e9d07e6993bc44bf17cdbdc05b3e
codemayor19 <codemayor19@gmail.com>
2023-10-23 06:48:36 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get node with index
+ * @head: struct
+ * @index: node index
+ * Return: struct
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	
+	if (head == NULL)
+		return (NULL);
+	while (head->next != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
