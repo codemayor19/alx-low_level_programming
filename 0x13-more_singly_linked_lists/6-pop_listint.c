a928a57c492f908ea84b6171f9c07e3d0e67cfc6
codemayor19 <codemayor19@gmail.com>
2023-10-23 06:23:51 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop list int
+ * @head: structure
+ * Return: integer
+ */
+int pop_listint(listint_t **head)
+{
+	int n; 
+	listint_t *tmp;
+
+	if (!head || !*head)
+		return (0);
+	n =( *head)->n;
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+	return (n);
+}	
