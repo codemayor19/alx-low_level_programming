08df740ce2e5820fcbc3b1011e724ddb2a4bd1dc
codemayor19 <codemayor19@gmail.com>
2023-10-23 07:51:06 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node using index
+ * @head: struct
+ * @index: index
+ * Return: 1 for success and -1 for failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
