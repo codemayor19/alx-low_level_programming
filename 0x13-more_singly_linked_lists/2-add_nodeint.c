f923f95ede10a73adfeb9a8e717bbc8b745f13dc
codemayor19 <codemayor19@gmail.com>
2023-10-23 04:36:22 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add a new node
+ * @head: the first node
+ * @n: value for the new node
+ * Return: address of the node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new =  malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
