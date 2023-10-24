35deae589db6f61fd021a5afceeba168369205fd
codemayor19 <codemayor19@gmail.com>
2023-10-24 13:01:00 -0700

    new commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,14 +7,14 @@
  */
 int pop_listint(listint_t **head)
 {
-	int n; 
+	int n;
 	listint_t *tmp;
 
 	if (!head || !*head)
 		return (0);
-	n =( *head)->n;
+	n = (*head)->n;
 	tmp = (*head)->next;
 	free(*head);
 	*head = tmp;
 	return (n);
-}	
+}
