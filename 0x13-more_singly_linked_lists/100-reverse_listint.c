db4ea934db9c4601ce8635ca76f27cd31fcaa60c
codemayor19 <codemayor19@gmail.com>
2023-10-24 13:27:31 -0700

    new commit

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -16,7 +16,7 @@ listint_t *reverse_listint(listint_t **head)
 		next = (*head)->next;
 		(*head)->next = prev;
 		prev = *head;
-		*head = next
+		*head = next;
 	}
 	*head = prev;
 	return (*head);
