1df1fdd1c19caedcec3e139a69d295899fa366c4
codemayor19 <codemayor19@gmail.com>
2023-10-31 02:38:39 -0700

    new commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,7 @@
  * error_file - check if a file is opened and handle error
  * @file_from: initial file to copy from and to
  * @file_to: destination file to copy to
+ * @argv: vector
  * Return: no return
  */
 void error_file(int file_from, int file_to, char *argv[])
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
 	ssize_t nchars, nwr;
-	char buff[1024];
+	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -44,10 +45,10 @@ int main(int argc, char *argv[])
 	nchars = 1024;
 	if (nchars == 1024)
 	{
-		nchars = read(file_from, buff, 1024);
+		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buff, nchars);
+		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
